a70fcc1a50dea8e49ce3a7c9720f54bc1d8956be
SadatArefin <sadat.arefin.rafat@gmail.com>
2023-06-03 10:54:23 +0600

    tutorial product component model services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddTutorialComponent } from './components/add-tutorial/add-tutorial.component';
+import { AddProductComponent } from './components/add-product/add-product.component';
+import { TutorialDetailsComponent } from './components/tutorial-details/tutorial-details.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { TutorialsListComponent } from './components/tutorials-list/tutorials-list.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTutorialComponent,
+    AddProductComponent,
+    TutorialDetailsComponent,
+    ProductDetailsComponent,
+    TutorialsListComponent,
+    ProductsListComponent
   ],
   imports: [
     BrowserModule,
