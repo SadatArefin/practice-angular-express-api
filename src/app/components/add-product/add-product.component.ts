3d281381c53da9d6dffcccdbbaa253116f9dffc2
SadatArefin <sadat.arefin.rafat@gmail.com>
2023-06-04 19:20:15 +0600

    add product component

--- src/app/components/add-product/add-product.component.ts
+++ src/app/components/add-product/add-product.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
+import { Product } from './../../models/product.model';
 
 @Component({
   selector: 'app-add-product',
   templateUrl: './add-product.component.html',
-  styleUrls: ['./add-product.component.css']
+  styleUrls: ['./add-product.component.css'],
 })
-export class AddProductComponent {
+export class AddProductComponent implements OnInit {
+  product: Product = {
+    name: '',
+    description: '',
+    price: 0,
+    quantity: 0,
+    status: false,
+  };
+  submitted = false;
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {}
+
+  saveProduct(): void {
+    const data = {
+      title: this.product.name,
+      description: this.product.description,
+    };
+
+    this.productService.create(data).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.submitted = true;
+      },
+      error: (e) => console.error(e),
+    });
+  }
+
+  newProduct(): void {
+    this.submitted = false;
+    this.product = {
+      name: '',
+      description: '',
+      price: 0,
+      quantity: 0,
+      status: false,
+    };
+  }
 }
--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -4,4 +4,5 @@ export class Product {
   description?: string;
   price?: Number;
   quantity?: Number;
+  status?: boolean;
 }
